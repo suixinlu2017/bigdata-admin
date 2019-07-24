cf39b0507761dc2f6af68f69bf34a7c18669eda9
chenyulong <872626491@qq.com>
2019-07-24 19:19:51 +0800

    Initial commit

---
+++ src/main/java/com/lx/bigdataadmin/BigdataAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lx.bigdataadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataAdminApplication.class, args);
+    }
+
+}
